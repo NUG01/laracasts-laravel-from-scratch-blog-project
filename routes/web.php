d3b96735f7cb886ccf8913b422652ba9e24562bc
NUG01 <nugoskhireli@redberry.ge>
2022-08-23 11:17:38 +0400

    xRevert "feat(categories): add dropdown functionality and logic"
    This reverts commit 574996bc6bd8005a4c55c1f6646f692b885b7f77.

--- routes/web.php
+++ routes/web.php
@@ -49,15 +49,11 @@ return view('post',[
 
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
-        'posts'=>$category->posts,
-        'currentCategory'=>$category,
-        'categories'=>Category::all()
+        'posts'=>$category->posts
     ]);
 });
 Route::get('authors/{author:username}',function(User $author){
     return view('posts',[
-        'posts'=>$author->posts,
-        'categories'=>Category::all()
-
+        'posts'=>$author->posts
     ]);
 });
